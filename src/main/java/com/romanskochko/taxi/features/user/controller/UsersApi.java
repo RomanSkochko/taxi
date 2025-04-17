6379e974c06005b65e80814e4a60e52f79a4a5af
RomanSkochko <romanslifestory@gmail.com>
2025-04-17 12:17:07 +0300

    refactor(user): change URL of createUser method

--- src/main/java/com/romanskochko/taxi/features/user/controller/UsersApi.java
+++ src/main/java/com/romanskochko/taxi/features/user/controller/UsersApi.java
@@ -2,7 +2,7 @@ package com.romanskochko.taxi.features.user.controller;
 
 public interface UsersApi {
     String BY_ID = "/api/v1/users/{id}";
-    String CREATE = "/api/v1/users/registration";
+    String CREATE = "/api/v1/users";
     String UPDATE = "/api/v1/users/{id}";
     String DELETE = "/api/v1/users/{id}";
     String CHANGE_PASSWORD = "/api/v1/users/change-password";
